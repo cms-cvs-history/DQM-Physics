d7b5e38998b9cffe6434353f8bd86e09f78bfd59
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2009-06-27 21:03:48 +0000

    fixed filenames to comply with standard convention introduce sequence

--- test/qcdPhotonsDQM_cfg.py
+++ test/qcdPhotonsDQM_cfg.py
@@ -1,11 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("QcdPhotonsPhotonJetDQM")
+process = cms.Process("QcdPhotonsDQM")
 
 process.load("DQMServices.Core.DQM_cfg")
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 
-process.dqmSaver.workflow = cms.untracked.string('/Physics/QcdPhotons_PhotonJet_DQM/PhotonJet')
+process.dqmSaver.workflow = cms.untracked.string('/My/Test/DataSet')
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
@@ -21,7 +21,7 @@ process.source = cms.Source("PoolSource",
 )
 
 # DQM monitor module for QCD-Photons: Photon+Jet
-process.QcdPhotonsAnalyzer = cms.EDAnalyzer("QcdPhotonsPhotonJetDQM",
+process.QcdPhotonsAnalyzer = cms.EDAnalyzer("QcdPhotonsDQM",
                             triggerPathToPass        = cms.string("HLT_Photon15_L1R"),
                             triggerResultsCollection = cms.InputTag("TriggerResults", "", "HLT"),
                             photonCollection         = cms.InputTag("photons"),
