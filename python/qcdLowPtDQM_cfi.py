15013dc7ee1c511df5325b4fac790a9f79dbdf80
cmsbuild <>
2009-12-04 12:36:43 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- Physics-V00-01-06'.
    Sprout from master 2009-11-24 11:14:40 UTC Michele De Gruttola <sha1-1affeab963765b0be73c528dcac93d9672777444@cern.ch> 'ewkMuLumiMonitor added'
    Cherrypick from master 2009-11-29 10:21:39 UTC Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch> 'Updated':
        src/QcdLowPtDQM.cc
        test/qcdLowPtDQM_cfg.py
    Cherrypick from master 2009-12-04 12:36:42 UTC Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch> 'Added new paths':
        python/qcdLowPtDQM_cfi.py
    Delete:
        interface/ElectronChecker.h
        interface/JetChecker.h
        interface/JetCombinatorics.h
        interface/KinematicsChecker.h
        interface/MEzCalculator.h
        interface/MetChecker.h
        interface/MuonChecker.h
        interface/Selection.h
        interface/SemiLeptonChecker.h
        plugins/DiLeptonsChecker.cc
        plugins/DiLeptonsChecker.h
        plugins/EventFilter.cc
        plugins/EventFilter.h
        plugins/LeptonJetsChecker.cc
        plugins/LeptonJetsChecker.h
        plugins/VertexChecker.cc
        plugins/VertexChecker.h
        src/ElectronChecker.cc
        src/JetChecker.cc
        src/JetCombinatorics.cc
        src/KinematicsChecker.cc
        src/MEzCalculator.cc
        src/MetChecker.cc
        src/MuonChecker.cc
        src/Selection.cc
        src/SemiLeptonChecker.cc

--- python/qcdLowPtDQM_cfi.py
+++ python/qcdLowPtDQM_cfi.py
@@ -1,4 +1,4 @@
-# $Id: qcdLowPtDQM_cfi.py,v 1.6 2009/11/19 22:32:41 loizides Exp $
+# $Id: qcdLowPtDQM_cfi.py,v 1.9 2009/11/29 11:20:45 loizides Exp $
 
 import FWCore.ParameterSet.Config as cms
 
@@ -23,17 +23,17 @@ myRecoSeq = cms.Sequence(myRecoSeq1 * myRecoSeq2)
 
 QcdLowPtDQM = cms.EDAnalyzer("QcdLowPtDQM",
     hltTrgNames  = cms.untracked.vstring(
-    'HLT_ZeroBias'
-#    'HLT_MinBias',
     'HLT_MinBiasBSC',
     'HLT_MinBiasBSC_OR',
     'HLT_MinBiasEcal',
     'HLT_MinBiasHcal',
     'HLT_MinBiasPixel_SingleTrack',
-    'HLT_HFThreshold',
+    'HLT_ZeroBiasPixel_SingleTrack',
+    'HLT_L1Tech_HCAL_HF_coincidence_PM',
+    'HLT_L1_BPTX',
+#    'HLT_HFThreshold3',
 #    'HLT_Activity_Ecal',
-    'HLT_Activity_EcalREM',
-    'HLT_Activity_PixelClusters',
+#    'HLT_Activity_PixelClusters',
     ),
                              
     verbose = cms.untracked.int32(3)
