6c6dff6b622f8fcfd12d95c9b40af7f9e153712a
Bastian Kargoll <sha1-884f4c47cc64aad1eab24ba7a310cc00dc717d22@cern.ch>
2010-03-25 17:59:00 +0000

    use DQMSaver, get rid of template

--- test/dileptonJetsChannel_cfg.py
+++ test/dileptonJetsChannel_cfg.py
@@ -1,15 +1,50 @@
-from DQM.Physics.validationTemplate_cfg import *
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TopDQM")
+
+## Message Logger (see: https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideMessageLogger for more information)
+process.MessageLogger = cms.Service("MessageLogger",
+	categories = cms.untracked.vstring('MainResults'
+#					  ,'Debug'
+	)
+)
+
+## source Input File
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
+  ## the following files are in for testing
+        '/store/relval/CMSSW_3_4_2/RelValProdTTbar/GEN-SIM-RECO/MC_3XY_V15-v1/0011/FE3B3C36-B413-DF11-9F84-002618FDA216.root',
+        '/store/relval/CMSSW_3_4_2/RelValProdTTbar/GEN-SIM-RECO/MC_3XY_V15-v1/0010/EC6B5FD5-6013-DF11-9795-001BFCDBD100.root',
+        '/store/relval/CMSSW_3_4_2/RelValProdTTbar/GEN-SIM-RECO/MC_3XY_V15-v1/0010/AE53FEB0-5F13-DF11-956B-001A928116C0.root',
+        '/store/relval/CMSSW_3_4_2/RelValProdTTbar/GEN-SIM-RECO/MC_3XY_V15-v1/0010/3621BC47-6013-DF11-9376-001731AF6A7D.root',
+        '/store/relval/CMSSW_3_4_2/RelValProdTTbar/GEN-SIM-RECO/MC_3XY_V15-v1/0010/280BA505-6513-DF11-867B-001A92971BB2.root',
+        '/store/relval/CMSSW_3_4_2/RelValProdTTbar/GEN-SIM-RECO/MC_3XY_V15-v1/0010/222EAB18-6B13-DF11-97D3-001A92971AEC.root',
+        '/store/relval/CMSSW_3_4_2/RelValProdTTbar/GEN-SIM-RECO/MC_3XY_V15-v1/0010/166D53B0-6613-DF11-A4DC-0030486791C6.root'
+
+  )
+)
+## define maximal number of events to loop over
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(200))
+
+## define output options
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+## DQM
+process.load("DQMServices.Core.DQM_cfg")
+process.load("DQMServices.Components.DQMEnvironment_cfi")
+process.DQM.collectorHost = ''
+
+process.dqmSaver.workflow = cms.untracked.string('/My/Test/DataSet')
 
 ## add jet corrections
 process.load("JetMETCorrections.Configuration.L2L3Corrections_Summer09_cff")
 process.prefer("L2L3JetCorrectorAK5Calo") 
 
 ## define process
-process.load("DQM.Physics.leptonJetsChecker_cfi")
+## process.load("DQM.Physics.leptonJetsChecker_cfi")
 process.load("DQM.Physics.dileptonJetsChecker_cfi")
 
 ## configure full-leptonic channels
-process.leptonJetsChecker.verbose      = False
+## process.leptonJetsChecker.verbose      = False
 #process.diLeptonsCheckerDiel.eidTight = 'eidRobustTight'
 #process.diLeptonsCheckerDiel.eidTight = 'eidTight'
 #process.diLeptonsCheckerDiel.eidTight = 'eidLoose'  ## this parameter does notv even exist!?!
@@ -32,30 +67,7 @@ process.p = cms.Path(
   process.metJESCorAK5CaloJet*
   process.diLeptonsCheckerDimu*
   process.diLeptonsCheckerDiel*
-  process.diLeptonsCheckerElmu
+  process.diLeptonsCheckerElmu*
+  process.dqmSaver
 )
-
-## ---
-##    replace input file here
-## ---
-#process.source.fileNames = [
-# '/store/relval/CMSSW_3_1_2/RelValTTbar/GEN-SIM-RECO/MC_31X_V3-v1/0007/CE243FB9-A778-DE11-8891-000423D98BC4.root',
-# '/store/relval/CMSSW_3_1_2/RelValTTbar/GEN-SIM-RECO/MC_31X_V3-v1/0006/D63BFAF8-5178-DE11-8439-001D09F24F65.root',
-# '/store/relval/CMSSW_3_1_2/RelValTTbar/GEN-SIM-RECO/MC_31X_V3-v1/0006/B8BA4AF7-5178-DE11-9F72-001D09F23A6B.root',
-# '/store/relval/CMSSW_3_1_2/RelValTTbar/GEN-SIM-RECO/MC_31X_V3-v1/0006/9C8FF416-5278-DE11-B809-0019B9F70607.root',
-# '/store/relval/CMSSW_3_1_2/RelValTTbar/GEN-SIM-RECO/MC_31X_V3-v1/0006/98079214-5278-DE11-8880-001D09F27067.root',
-# '/store/relval/CMSSW_3_1_2/RelValTTbar/GEN-SIM-RECO/MC_31X_V3-v1/0006/62B58021-5278-DE11-9574-0019B9F6C674.root',
-# '/store/relval/CMSSW_3_1_2/RelValTTbar/GEN-SIM-RECO/MC_31X_V3-v1/0006/3AEF4E28-5278-DE11-A2D4-000423D6CA72.root'
-#     ]
-
-## ---
-##    replace maximal number of events here
-## ---
-process.maxEvents.input = -1
-
-## ---
-## replace option arguments here
-## ---
-## process.options.wantSummary = False
-
 
