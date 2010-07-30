f838c30f2ecd7717246b230a3ce3f06183fe51fe
cmsbuild <>
2010-07-30 16:36:56 +0000

    This commit was manufactured by cvs2git to create tag 'DQM-Physics- wmtusing'.
    Sprout from master 2010-05-18 20:37:43 UTC Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch> 'moved verbosity level from STANDARD to DEBUG -- this is not so many histograms after all'
    Cherrypick from master 2010-07-30 16:36:53 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Add reco:: namespace qualifiers':
        python/DQMPhysics_cff.py
        src/BPhysicsOniaDQM.h
        src/EwkElecDQM.cc
        src/EwkMuDQM.cc
        src/EwkMuLumiMonitorDQM.cc
        src/QcdHighPtDQM.h
        src/QcdUeDQM.cc

--- src/QcdHighPtDQM.h
+++ src/QcdHighPtDQM.h
@@ -14,6 +14,7 @@
 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Utilities/interface/InputTag.h"
 #include "DataFormats/JetReco/interface/CaloJet.h"
 #include "DataFormats/JetReco/interface/CaloJetCollection.h"
 #include "DataFormats/METReco/interface/CaloMETCollection.h"
@@ -59,8 +60,8 @@ class QcdHighPtDQM : public edm::EDAnalyzer {
   std::map<std::string, MonitorElement*> MEcontainer_;
 
   //methods to calculate MET over SumET and MET over Leading Jet Pt
-  float movers(const CaloMETCollection& metcollection);
-  float moverl(const CaloMETCollection& metcollection, float& ljpt);
+  float movers(const reco::CaloMETCollection& metcollection);
+  float moverl(const reco::CaloMETCollection& metcollection, float& ljpt);
 
 };
 #endif  
